ec9ba63ca9b4016bcd9eb2e05586b05fa0ec82f6
俞云烽 <1957689514@qq.com>
2025-01-12 22:56:00 +0800

    feat(user): 新增用户信息更新、头像更新及密码更新功能
    目前为止已经完成了用户模块的开发。
    
    Signed-off-by: 俞云烽 <1957689514@qq.com>

--- src/main/java/com/auroral/controller/UserController.java
+++ src/main/java/com/auroral/controller/UserController.java
@@ -7,7 +7,9 @@ import com.auroral.utils.JwtUtil;
 import com.auroral.utils.Md5Util;
 import com.auroral.utils.ThreadLocalUtil;
 import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.URL;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -63,4 +65,39 @@ public class UserController {
         User user = userService.findByUserName(username);
         return Result.success(user);
     }
+    @PutMapping("/update")
+    public Result update(@RequestBody @Validated User user){
+        userService.update(user);
+        return Result.success();
+    }
+    @PatchMapping("/updateAvatar")
+    public Result updateAvatar(@RequestParam @URL String avatarUrl){
+        userService.updateAvatar(avatarUrl);
+        return Result.success();
+    }
+    @PatchMapping("/updatePwd")
+    public Result updatePwd(@RequestBody Map<String,String> params){
+        //1.校验参数
+        String oldPwd = params.get("old_pwd");
+        String newPwd = params.get("new_pwd");
+        String rePwd = params.get("re_pwd");
+        if(!StringUtils.hasLength(oldPwd) || !StringUtils.hasLength(newPwd) || !StringUtils.hasLength(rePwd)){
+            return Result.error("缺少必要的参数");
+        }
+        //原密码是否正确
+        //调用userService根据用户名拿到原密码，再和old_pwd对比
+        Map<String,Object> map = ThreadLocalUtil.get();
+        String username = (String) map.get("username");
+        User longinUser = userService.findByUserName(username);
+        if(!longinUser.getPassword().equals(Md5Util.getMD5String(oldPwd))){
+            return Result.error("原密码填写不正确");
+        }
+        //newpwd密码和re_pwd是否一致
+        if(!newPwd.equals(rePwd)){
+            return Result.error("两次填写的新密码不一致");
+        }
+        //2.调用Service完成密码更新
+        userService.updatePwd(newPwd);
+        return Result.success();
+        }
 }
--- src/main/java/com/auroral/mapper/UserMapper.java
+++ src/main/java/com/auroral/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import com.auroral.pojo.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface UserMapper {
@@ -13,6 +14,14 @@ public interface UserMapper {
 
     @Insert("INSERT INTO user (username, password,create_time, update_time" +
             ") VALUES (#{username}, #{password}, NOW(), NOW())")
-    // 添加用户信息
+        // 添加用户信息
     void add(String username, String password);
+
+    @Update("UPDATE user SET nickname = #{nickname}, email = #{email},  update_time = #{updateTime} WHERE id = #{id}")
+    void update(User user);
+    @Update("UPDATE user SET user_pic = #{avatarUrl} , update_time =NOW() WHERE id = #{id}")
+    void updateAvatar(String avatarUrl,Integer id);
+
+    @Update("UPDATE user SET password = #{password}  , update_time =NOW()WHERE id = #{id}")
+    void updatePwd(String password, Integer id);
 }

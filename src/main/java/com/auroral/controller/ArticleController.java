657f471c075068af4594486491392da11f16086a
俞云烽 <1957689514@qq.com>
2025-01-13 11:19:29 +0800

    feat(article): 新增文章管理功能，包括增删改查和状态校验等
    Signed-off-by: 俞云烽 <1957689514@qq.com>

--- src/main/java/com/auroral/controller/ArticleController.java
+++ src/main/java/com/auroral/controller/ArticleController.java
@@ -1,25 +1,47 @@
 package com.auroral.controller;
 
 
+import com.auroral.pojo.Article;
+import com.auroral.pojo.PageBean;
 import com.auroral.pojo.Result;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.auroral.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/article")
 public class ArticleController {
-    @GetMapping("/list")
-    public Result<String> list() {
-//        //验证token
-//        try {
-//            Map<String, Object> claims = JwtUtil.parseToken(token);
-//            return Result.success("所有的文章数据.....");
-//        } catch (Exception e) {
-//            //http响应状态吗为401，表示token验证失败
-//            response.setStatus(401);
-//            return Result.error("未登录");
-//        }
-        return Result.success("所有的文章数据.....");
+    @Autowired
+    private ArticleService articleService;
+    @PostMapping
+        public Result add(@RequestBody @Validated Article article) {
+        articleService.add(article);
+        return Result.success();
+    }
+    @GetMapping
+    public Result<PageBean<Article>> list(
+            Integer pageNum,
+            Integer pageSize,
+            @RequestParam(required = false) Integer categoryId,
+            @RequestParam(required = false) String state
+    ){
+        PageBean<Article> pb = articleService.list(pageNum, pageSize, categoryId, state);
+        return Result.success(pb);
+    }
+    @GetMapping("/detail")
+    public Result<Article> detail( Integer id) {
+        Article article = articleService.findById(id);
+        return Result.success(article);
+    }
+    @PutMapping
+    public Result update(@RequestBody @Validated Article article) {
+        articleService.update(article);
+        return Result.success();
+    }
+    @DeleteMapping
+    public Result delete(Integer id) {
+        articleService.delete(id);
+        return Result.success();
     }
 }

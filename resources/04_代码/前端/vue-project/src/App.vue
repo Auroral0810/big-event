38c68b62b3308d901fabf1cabb8a83f49d62730f
俞云烽 <1957689514@qq.com>
2025-01-14 15:01:14 +0800

    添加了课件资料

---
+++ resources/04_代码/前端/vue-project/src/App.vue
@@ -0,0 +1,40 @@
+<!-- <script>
+  //写数据
+  export default{
+    data(){
+      return {
+        msg:'上海'
+      }
+    }
+  }
+</script> -->
+<script setup>
+  import {ref} from 'vue';
+  //调用ref函数,定义响应式数据
+  const msg = ref('西安');
+
+  //导入 Api.vue文件
+  import ApiVue from './Api.vue'
+  // 导入Article.vue文件
+  // import ArticleVue from './Article.vue'
+  //导入Article1.vue文件
+  import Article1Vue from './Article1.vue'
+</script>
+
+<template>
+  <!-- html -->
+  <!-- <h1>北京</h1> -->
+  <!-- <h1>{{ msg }}</h1>
+  <br>
+  <ApiVue/> -->
+
+  <Article1Vue/>
+  <!-- <ArticleVue/> -->
+</template>
+
+<style scoped>
+  /* 样式 */
+  h1{
+    color: red;
+  }
+</style>
